1a4f32d1d82f9b5bae2c31eb078bdad495be58f0
JD Davis <mxjerrett@gmail.com>
2025-01-11 20:59:00 -0600

    feat: added vial support and corrected QMK/VIA RGB Matrix for Tide75

--- keyboards/epomaker/tide75/keymaps/vial/keymap.c
+++ keyboards/epomaker/tide75/keymaps/vial/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,      KC_BSLS,   KC_END,
              KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT,        KC_ENT,   KC_PGUP,
               KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,            KC_RSFT,     KC_UP, KC_PGDN,
-          KC_LCTL, KC_LGUI,  KC_LALT,                       KC_SPC                       , KC_RALT,   MO(1), KC_RCTL,        KC_LEFT,  KC_DOWN, KC_RGHT
+          KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_SPC, KC_SPC, KC_RALT, MO(1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT( /* Base */
@@ -21,12 +21,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_SAD,  RGB_SAI,    RGB_MOD,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUD, RGB_HUI,  _______,  _______,            RGB_VAI,  _______,
-        _______,  GU_TOGG,  _______,                     EE_CLR                        , _______,  _______,  _______,  RGB_SPD,  RGB_VAD,  RGB_SPI
+        _______, GU_TOGG, _______, EE_CLR, _______, EE_CLR, EE_CLR, _______, _______, _______,RGB_SPD,  RGB_VAD,  RGB_SPI
     )
 };
 
 #ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [1] = {ENCODER_CCW_CW(KC_WH_D, KC_WH_U)},
 };
