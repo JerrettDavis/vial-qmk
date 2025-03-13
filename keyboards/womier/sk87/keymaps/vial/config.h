10e6581ed3795d17dede8bb6d160c49d24360ced
Jerrett D. Davis <mxjerrett@gmail.com>
2025-03-12 23:37:34 -0500

    feat: adding vial support for sk87

---
+++ keyboards/womier/sk87/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x30, 0x53, 0x22, 0xF2, 0xE5, 0xD0, 0xBF, 0x06}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
